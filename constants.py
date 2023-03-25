9909018685864fdcfbde24b1cccb624c707d3e4b
crnkyunicorn <james@crankyunicorn.com>
2023-03-25 15:09:35 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,12 +1,16 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-PRODUCTION_ENV_ACCOUNT = "" # which region would you like to deploy the production studio
-PRODUCTION_ENV_REGION = "" # which region would you like to deploy the production studio
+PRODUCTION_ENV_ACCOUNT = (
+    ""  # which region would you like to deploy the production studio
+)
+PRODUCTION_ENV_REGION = (
+    ""  # which region would you like to deploy the production studio
+)
 
-TOOLCHAIN_ACCOUNT = "" # which account should your cicd pipelines go in.
-TOOLCHAIN_REGION = "" # which region should your cicd pipelines go in.
+TOOLCHAIN_ACCOUNT = ""  # which account should your cicd pipelines go in.
+TOOLCHAIN_REGION = ""  # which region should your cicd pipelines go in.
 
-VPC_NAME = "" # update to the VPC you will install SageMaker Studio into.
+VPC_NAME = ""  # update to the VPC you will install SageMaker Studio into.
 
 # Environment variables
 APP_NAME = "sagemaker-studio"
